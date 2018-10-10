9d480af923fcc3e8460e0d30870ff9c9556eb142
Frederikke Grøndal Schjøtt <eaafgs@students.eaaa.dk>
2018-10-10 12:10:52 +0200

    popup links

---
+++ hallohostel/script/hangouts.js
@@ -0,0 +1,66 @@
+/* event 1 join button*/
+document.querySelector('.j-icon1').addEventListener('click',
+function() {
+  document.querySelector('.j-popup').style.display = 'flex';
+});
+
+document.querySelector('.j-close').addEventListener('click',
+function() {
+  document.querySelector('.j-popup').style.display = 'none';
+});
+
+/* event 2 join button*/
+document.querySelector('.j-icon2').addEventListener('click',
+function() {
+  document.querySelector('.j-popup').style.display = 'flex';
+});
+
+document.querySelector('.j-close').addEventListener('click',
+function() {
+  document.querySelector('.j-popup').style.display = 'none';
+});
+
+/* event 3 join button*/
+document.querySelector('.j-icon3').addEventListener('click',
+function() {
+  document.querySelector('.j-popup').style.display = 'flex';
+});
+
+document.querySelector('.j-close').addEventListener('click',
+function() {
+  document.querySelector('.j-popup').style.display = 'none';
+});
+
+
+/* event 1 participants button*/
+document.querySelector('.p-icon1').addEventListener('click',
+function() {
+  document.querySelector('.p-popup').style.display = 'flex';
+});
+
+document.querySelector('.p-close').addEventListener('click',
+function() {
+  document.querySelector('.p-popup').style.display = 'none';
+});
+
+/* event 2 participants button*/
+document.querySelector('.p-icon2').addEventListener('click',
+function() {
+  document.querySelector('.p-popup').style.display = 'flex';
+});
+
+document.querySelector('.p-close').addEventListener('click',
+function() {
+  document.querySelector('.p-popup').style.display = 'none';
+});
+
+/* event 3 participants button*/
+document.querySelector('.p-icon3').addEventListener('click',
+function() {
+  document.querySelector('.p-popup').style.display = 'flex';
+});
+
+document.querySelector('.p-close').addEventListener('click',
+function() {
+  document.querySelector('.p-popup').style.display = 'none';
+});
